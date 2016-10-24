8a9f404e3a88cd2e31c890a855ca04ef20bb570b
kkeisenman <kevinke@okstate.edu>
2016-10-24 17:04:50 -0500

    Add project files.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4003.Startup))]
+namespace _4003
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
